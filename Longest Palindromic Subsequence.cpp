7801ac0f9ca6aa2fc6ae62e114477e88b65e4a3e
UtkarshaGupta767 <76815914+UtkarshaGupta767@users.noreply.github.com>
2020-12-31 20:19:39 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Longest Palindromic Subsequence.cpp
@@ -0,0 +1,43 @@
+int lcs(string A,string B)
+{
+    int n=A.size();
+int m=B.size();
+int t[n+1][m+1];
+for(int i=0;i<n+1;i++)
+{
+    for(int j=0;j<m+1;j++)
+      {
+          if(i==0||j==0)
+             t[i][j]=0;
+      }
+}
+for(int i=1;i<n+1;i++)
+{
+    for(int j=1;j<m+1;j++)
+       {
+           if(A[i-1]==B[j-1])
+             {
+                 t[i][j]=1+t[i-1][j-1];
+             }
+             else
+             {
+                 t[i][j]=max(t[i-1][j],t[i][j-1]);
+             }
+       }
+}
+
+return t[n][m];
+}
+
+
+
+
+int Solution::solve(string A) {
+    
+    string B=A;
+    reverse(A.begin(),A.end());
+    int ans=lcs(A,B);
+    return ans;
+    
+    
+}
